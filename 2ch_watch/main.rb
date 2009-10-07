8f34c9b3708cdb74cf4f8813b3bdf23dc5317ddb
hitode909 <hitode909@gmail.com>
2009-10-07 11:56:08 +0900

    wrote

--- 2ch_watch/main.rb
+++ 2ch_watch/main.rb
@@ -1,8 +1,32 @@
+# -*- coding: utf-8 -*-
 $:.unshift(File.dirname(__FILE__) + '/../modules/bot2ch/')
+$:.unshift(File.dirname(__FILE__) + '/../lib/')
 require 'bot2ch'
 require 'pstore'
 require 'tmpdir'
+require 'sanitize'
+require 'cgi'
+require 'api'
+
+def escape_body body
+  CGI.unescapeHTML(body.gsub(/<[^>]*>/,'')).strip.gsub(/\s+|　/, ' ')
+end
 
 APPLICATION = '2ch_watch'
 api = Api.new(APPLICATION)
+db = PStore.new("#{Dir.tmpdir}/#{APPLICATION}")
 
+menu = Bot2ch::Menu.new
+board = menu.get_board '/net/'
+threads = board.threads.select{|th| th.title =~ /Twitter/}
+threads.each{ |th|
+  db.transaction {
+    last_fetched = db[th.dat_no]
+    next if th.posts.length == last_fetched
+    from = last_fetched ? last_fetched + 1 : 0
+    th.posts[from..-1].each { |post|
+      api.update "#{post.index}: #{escape_body post.body} #{post.url}"
+    }
+    db[th.dat_no] = th.posts.length
+  }
+}
--- 2ch_watch/sketch.rb
+++ 2ch_watch/sketch.rb
@@ -7,8 +7,14 @@ menu = Bot2ch::Menu.new
 board = menu.get_board('/net/')
 threads = board.threads.select{|th| th.title =~ /Twitter/}
 
+p threads.first
+puts threads.first.url
+puts threads.first.posts.first.url
+exit
+
 threads.each{ |th|
   post = th.posts.last
   puts "#{post.index}: #{post.body}"
   p post
+  puts post.threa
 }
